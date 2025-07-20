36e10b563fa7039784d3c726fa4f4d4a712b8f44
Arthur05-FT <arthurfotso.bfkg@gmail.com>
2025-07-20 15:11:19 +0100

    creation and stylizing of the hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/Header";
+import {Background} from "@/components/Background";
 
 const inter = Inter({
     subsets: ["latin"]
@@ -19,8 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={inter.className}
+        className={`${inter.className} relative h-screen`}
       >
+        <Header />
+        <Background />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Hero from "@/components/landing-components/Hero";
+import {ArrowRightIcon} from "lucide-react";
+import Link from "next/link";
+import {SparklesText} from "@/components/magicui/sparkles-text";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div className="flex pt-[4rem] flex-col items-center">
+    <SparklesText className="text-[16px] font-light">
+      <Link href={"/"} className="inline-flex items-center inset-shadow-sm gap-1 px-4 py-1 rounded-full">✨ Introducing Todo task <ArrowRightIcon size={16} /></Link>
+    </SparklesText>
+    <Hero />
+  </div>;
 };
 
 export default Home;
